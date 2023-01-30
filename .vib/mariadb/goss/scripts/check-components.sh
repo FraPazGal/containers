b8cc4bedb06e017c82ce71f9a5bb156e6963a23f
FraPazGal <fdepaz@vmware.com>
2023-01-30 14:29:26 +0100

    Fix goss tests
    Signed-off-by: FraPazGal <fdepaz@vmware.com>

--- .vib/mariadb/goss/scripts/check-components.sh
+++ .vib/mariadb/goss/scripts/check-components.sh
@@ -3,10 +3,10 @@
 components=( $(cat /opt/bitnami/.bitnami_components.json | grep ": {" | cut -d ':' -f 1 | tr -d '"') )
 
 for component in "${components[@]}"; do
-  if "$component" == "mariadb"; then
-    which -s mysql
+  if [[ "$component" == "mariadb" ]]; then
+    which mysql
   else
-    which -s $component
+    which $component
   fi
   [ $? -eq 0 ] || exit 1
 done
