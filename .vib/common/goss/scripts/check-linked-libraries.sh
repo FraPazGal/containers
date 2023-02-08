1c2e6c507441e6a0c5a8a3256bfb6438063a2359
Francisco de Paz Galan <fdepaz@vmware.com>
2023-02-08 12:21:48 +0100

    Apply suggestions from code review
    Co-authored-by: Jose Antonio Carmona <joancafom@icloud.com>
    Signed-off-by: Francisco de Paz Galan <fdepaz@vmware.com>

--- .vib/common/goss/scripts/check-linked-libraries.sh
+++ .vib/common/goss/scripts/check-linked-libraries.sh
@@ -3,7 +3,5 @@
 read -r -a files <<< "$(find "$BITNAMI_ROOT_DIR"/"$BITNAMI_APP_NAME" "$BITNAMI_ROOT_DIR"/common -type f -executable)"
 
 for file in "${files[@]}"; do
-  if [[ ! -x "$file" ]] || [[ $(ldd "$file" | grep -c "not found") -gt 0 ]]; then
-    exit 1
-  fi
+  [[ $(ldd "$file" | grep -c "not found") -gt 0 ]] && exit 1
 done
